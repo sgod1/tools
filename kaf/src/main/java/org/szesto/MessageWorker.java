901eb7d02d53fa9126d0e38c3df64a39cbc5a40c
Simon Godik <simon@godik.com>
2026-06-25 11:35:20 -0700

    serializers

--- kaf/src/main/java/org/szesto/MessageWorker.java
+++ kaf/src/main/java/org/szesto/MessageWorker.java
@@ -65,10 +65,6 @@ public class MessageWorker {
     }
 
     public static KafkaProducer<String, String> createProducer(Properties props) {
-
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
         return new KafkaProducer<>(props);
     }
 
